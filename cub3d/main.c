aa9044a9b3ee76454c3d29130601ef4852841b92
dhkham <khamdong@gmail.com>
2023-09-09 17:44:22 +0900

    splitting functions

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 18:41:21 by chanwoki          #+#    #+#             */
-/*   Updated: 2023/09/09 16:45:23 by dkham            ###   ########.fr       */
+/*   Updated: 2023/09/09 17:43:57 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,79 +54,85 @@ void	free_config(t_config *config)
 	}
 }
 
-void initialize_s_info(t_info *info, t_config *config)
+void find_start_position(t_info *info, t_config *config, int *i, int *j)
 {
-	int i;
-	int j;
+    *i = 0;
+    while (*i < config->map_height)
+    {
+        *j = 0;
+        while (*j < config->map_width)
+        {
+            if (config->map[*i][*j] == 'N' || config->map[*i][*j] == 'S' || config->map[*i][*j] == 'W' || config->map[*i][*j] == 'E')
+            {
+                info->pos_x = *i + 0.5;
+                info->pos_y = *j + 0.5;
+                return;
+            }
+            (*j)++;
+        }
+        (*i)++;
+    }
+}
 
-	printf("set start position and direction\n");
-	i = 0; //j = 0;
-	while (i < config->map_height)
-	{
-		j = 0;
-		while (j < config->map_width)
-		{
-			if (config->map[i][j] == 'N' || config->map[i][j] == 'S' || config->map[i][j] == 'W' || config->map[i][j] == 'E')
-			{
-				info->pos_x = i + 0.5;
-				info->pos_y = j + 0.5;
-			}
-			if (config->map[i][j] == 'N')
-			{
-				info->dir_x = -1.0;
-				info->dir_y = 0.0;
-				info->plane_x = 0.0;
-				info->plane_y = 0.66;
-				config->map[i][j] = '0';
-				printf("NORTH\n");
-				break ;
-			}
-			else if (config->map[i][j] == 'S')
-			{
-				info->dir_x = 1.0;
-				info->dir_y = 0.0;
-				info->plane_x = 0.0;
-				info->plane_y = -0.66;
-				config->map[i][j] = '0';
-				printf("SOUTH\n");
-				break ;
-			}
-			else if (config->map[i][j] == 'W')
-			{
-				info->dir_x = 0.0;
-				info->dir_y = -1.0;
-				info->plane_x = -0.66;
-				info->plane_y = 0.0;
-				config->map[i][j] = '0';
-				printf("WEST\n");
-				break ;
-			}
-			else if (config->map[i][j] == 'E')
-			{
-				info->dir_x = 0.0;
-				info->dir_y = 1.0;
-				info->plane_x = 0.66;
-				info->plane_y = 0.0;
-				config->map[i][j] = '0';
-				printf("EAST\n");
-				break ;
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("info->pos_x: %f\n", info->pos_x);
-	printf("info->pos_y: %f\n", info->pos_y);
-	printf("info->dir_x: %f\n", info->dir_x);
-	printf("info->dir_y: %f\n", info->dir_y);
-	printf("info->plane_x: %f\n", info->plane_x);
-	printf("info->plane_y: %f\n", info->plane_y);
+void set_direction_north(t_info *info, t_config *config, int i, int j)
+{
+    info->dir_x = -1.0;
+    info->dir_y = 0.0;
+    info->plane_x = 0.0;
+    info->plane_y = 0.66;
+    config->map[i][j] = '0';
+}
 
-    // Initialize mlx and win pointers
+void set_direction_south(t_info *info, t_config *config, int i, int j)
+{
+    info->dir_x = 1.0;
+    info->dir_y = 0.0;
+    info->plane_x = 0.0;
+    info->plane_y = -0.66;
+    config->map[i][j] = '0';
+}
+
+void set_direction_west(t_info *info, t_config *config, int i, int j)
+{
+    info->dir_x = 0.0;
+    info->dir_y = -1.0;
+    info->plane_x = -0.66;
+    info->plane_y = 0.0;
+    config->map[i][j] = '0';
+}
+
+void set_direction_east(t_info *info, t_config *config, int i, int j)
+{
+    info->dir_x = 0.0;
+    info->dir_y = 1.0;
+    info->plane_x = 0.66;
+    info->plane_y = 0.0;
+    config->map[i][j] = '0';
+}
+
+void set_direction_and_plane(t_info *info, t_config *config, int i, int j)
+{
+    if (config->map[i][j] == 'N')
+        set_direction_north(info, config, i, j);
+    else if (config->map[i][j] == 'S')
+        set_direction_south(info, config, i, j);
+    else if (config->map[i][j] == 'W')
+        set_direction_west(info, config, i, j);
+    else if (config->map[i][j] == 'E')
+        set_direction_east(info, config, i, j);
+}
+
+void set_start_position_and_direction(t_info *info, t_config *config)
+{
+    int i, j;
+    find_start_position(info, config, &i, &j);
+    set_direction_and_plane(info, config, i, j);
+}
+
+void initialize_basic_fields(t_info *info, t_config *config)
+{
     info->mlx = NULL;
     info->win = NULL;
-
-    // Initialize image
     info->img.img = NULL;
     info->img.data = NULL;
     info->img.size_l = 0;
@@ -134,70 +140,79 @@ void initialize_s_info(t_info *info, t_config *config)
     info->img.endian = 0;
     info->img.img_width = 0;
     info->img.img_height = 0;
-
-	// Initialize config
 	info->config = *config;
-	printf("info->config.map_width: %d\n", info->config.map_width);
-	printf("info->config.map_height: %d\n", info->config.map_height);
-	//printf("info->config.direction: %s\n", info->config.direction);
-	printf("info->config.north_texture: %s\n", info->config.north_texture);
-	printf("info->config.south_texture: %s\n", info->config.south_texture);
-	printf("info->config.west_texture: %s\n", info->config.west_texture);
-	printf("info->config.east_texture: %s\n", info->config.east_texture);
+	info->move_speed = 0.1;
+    info->rot_speed = 0.1;
+}
 
-	// Initialize buffer - considering height and width are constants
-	int x;
+void initialize_buffer(t_info *info)
+{
+    int x;
 	int y;
 
-	y = 0;
-	while (y < HEIGHT)
-	{
-		x = 0;
-		while (x < WIDTH)
-		{
-			info->buf[y][x] = 0;
-			x++;
-		}
-		y++;
-	}
-    // Initialize texture - Assuming a total of 4 textures based on previous info
+    y = 0;
+    while (y < HEIGHT)
+    {
+        x = 0;
+        while (x < WIDTH)
+        {
+            info->buf[y][x] = 0;
+            x++;
+        }
+        y++;
+    }
+}
+
+void initialize_texture(t_info *info)
+{
+    int x;
+	int y;
+	int i;
+
     if (!(info->texture = (int **)malloc(sizeof(int *) * 4)))
-	{
-		perror("Error allocating memory for texture");
+    {
+        perror("Error allocating memory for texture");
 		// free_resources(info); // 확인
-		// free_config(config);
-		exit(1);
-	}
-	i = 0;
-	while (i < 4)	// 텍스처의 각 행에 대한 메모리 할당
-	{
-		if (!(info->texture[i] = (int *)malloc(sizeof(int) * (TEXTURE_WIDTH * TEXTURE_HEIGHT))))
-		{
-			perror("Error allocating memory for texture");
+ 		// free_config(config);
+        exit(1);  // You might want to replace this with proper error handling
+    }
+    i = 0;
+    while (i < 4)    // Memory allocation for each row of the texture
+    {
+        if (!(info->texture[i] = (int *)malloc(sizeof(int) * (TEXTURE_WIDTH * TEXTURE_HEIGHT))))
+        {
+            perror("Error allocating memory for texture row");
 			// free_resources(info);
 			// free_config(config);
-			exit(1);
-		}
-		i++;
-	}
-	i = 0;
-	while (i < 4)	// 텍스처 초기화
-	{
-		y = 0;
-		while (y < TEXTURE_HEIGHT)
-		{
-			x = 0;
-			while (x < TEXTURE_WIDTH)
-			{
-				info->texture[i][TEXTURE_WIDTH * y + x] = 0;
-				x++;
-			}
-			y++;
-		}
-		i++;
-	}
-    info->move_speed = 0.1;
-    info->rot_speed = 0.1;
+            exit(1);  // You might want to replace this with proper error handling
+        }
+        i++;
+    }
+    i = 0;
+    while (i < 4)    // Initialize the texture
+    {
+        y = 0;
+        while (y < TEXTURE_HEIGHT)
+        {
+            x = 0;
+            while (x < TEXTURE_WIDTH)
+            {
+                info->texture[i][TEXTURE_WIDTH * y + x] = 0;
+                x++;
+            }
+            y++;
+        }
+        i++;
+    }
+}
+
+
+void initialize_s_info(t_info *info, t_config *config)
+{
+	set_start_position_and_direction(info, config);
+	initialize_basic_fields(info, config);
+	initialize_buffer(info);
+	initialize_texture(info);
 }
 
 void free_resources(t_info *info)
@@ -235,7 +250,7 @@ int load_image(t_info *info, int *texture, char *path, t_img *img)
 		y++;
 	}
     mlx_destroy_image(info->mlx, img->img);
-    return (1); // Return 1 to indicate success
+    return (1);
 }
 
 int load_texture_from_config(t_info *info, t_config *config)
@@ -355,19 +370,20 @@ void raycasting(t_info *info)
 		// 광선이 부딪힌 벽의 방향에 따라 사용할 텍스처를 결정
 		// 방향에 대한 확인 필요
 		int texNum;
-		if (side == 0)   // x면에 부딪혔다면
+		if (side == 0)   // If ray hit a wall in the x-direction
 		{
-			if (rayDirX > 0)
-				texNum = 2;  // West
+			//printf("if side = 0\n");
+			if (rayDirX > 0) // If ray is pointing to the right
+				texNum = 1;	// S
 			else
-				texNum = 3;  // East
+				texNum = 0; // N
 		}
-		else  			// y면에 부딪혔다면
-		{
-			if (rayDirY > 0)
-				texNum = 0;  // North
+		else  // If ray hit a wall in the y-direction
+		{	//printf("if side != 0\n");	
+			if (rayDirY > 0) // If ray is pointing downwards
+				texNum = 3;	// E
 			else
-				texNum = 1;  // South
+				texNum = 2; // W
 		}
 		double wallX; // wallX 계산: 벽에 부딪힌 위치 계산 (텍스처 매핑을 위해)
 		if (side == 0)										//만약 광선이 X면에 부딪혔다면
@@ -399,6 +415,12 @@ void raycasting(t_info *info)
 	}
 }
 
+
+// void raycasting(t_info *info) {
+//     cast_rays(info);
+//     render_walls(info);
+// }
+
 void drawing(t_info *info)
 {
 	for (int y = 0; y < HEIGHT; y++) // row (height) 훑기
@@ -420,159 +442,6 @@ int game_loop(t_info *info)
 	drawing(info);
 	return (0);
 }
-// int game_loop(t_info *info)//, t_config *config)
-// {
-// 	int y;
-// 	int x;
-
-// 	y = 0;
-// 	while (y < HEIGHT) // 천장, 바닥 색칠하기
-// 	{
-// 		x = 0;
-// 		while (x < WIDTH)
-// 		{
-// 			info->buf[y][x] = rgb_to_int(info->config.floor_color[0], info->config.floor_color[1], info->config.floor_color[2]);	// floor
-// 			info->buf[HEIGHT - y - 1][x] = rgb_to_int(info->config.ceiling_color[0], info->config.ceiling_color[1], info->config.ceiling_color[2]);	// ceiling
-// 			x++;
-// 		}
-// 		y++;
-// 	}
-// 	for (int x = 0; x < WIDTH; x++)
-// 	{
-// 		double cameraX = 2 * x / (double)WIDTH - 1;
-// 		double rayDirX = info->dir_x + info->plane_x * cameraX;
-// 		double rayDirY = info->dir_y + info->plane_y * cameraX;
-
-// 		int mapX = (int)info->pos_x;								//레이의 위치 (여기선 일단 시작 지점 맵상의 x,y좌표)
-// 		int mapY = (int)info->pos_y;
-
-// 		double sideDistX;										// 레이가 x, y면에 부딪히기까지의 거리
-// 		double sideDistY;
-
-// 		double deltaDistX = fabs(1 / rayDirX);					// 레이가 x, y면에 부딪히기까지의 거리
-// 		double deltaDistY = fabs(1 / rayDirY);
-// 		double perpWallDist;									//플레이어 위치부터 레이가 벽과 충돌한 지점까지의 직교 거리
-
-// 		int stepX;												//x, y 방향으로 얼마나 가야하는지 (만약 rayDirX/Y가 음수면 -1, 아니면 1) : mapX/Y를 업데이트 위해 사용
-// 		int stepY;
-
-// 		int hit = 0; 											//was there a wall hit?
-// 		int side; 												//was a NS or a EW wall hit?
-
-// 		if (rayDirX < 0) //만약 레이가 -x 방향으로 향하고 있다면,
-// 		{
-// 			stepX = -1;	//mapX는 현재 위치보다 1 작은 값이 됨
-// 			sideDistX = (info->pos_x - mapX) * deltaDistX;		//mapX+1-pos_x : 1 = sideDistX : deltaDistX
-// 		}
-// 		else			//만약 레이가 -x 방향으로 향하고 있다면,
-// 		{
-// 			stepX = 1;	//mapX는 현재 위치보다 1 큰 값이 됨
-// 			sideDistX = (mapX + 1.0 - info->pos_x) * deltaDistX;	//pos_x-mapx : 1 = sideDistX : deltaDistX
-// 		}
-// 		if (rayDirY < 0)
-// 		{
-// 			stepY = -1;
-// 			sideDistY = (info->pos_y - mapY) * deltaDistY;
-// 		}
-// 		else
-// 		{
-// 			stepY = 1;
-// 			sideDistY = (mapY + 1.0 - info->pos_y) * deltaDistY;
-// 		}
-// 		while (hit == 0) //DDA 알고리즘 : 광선이 부딪힐 때까지 계속 광선을 한 칸씩 이동
-// 		{
-// 			//jump to next map square, OR in x-direction, OR in y-direction
-// 			if (sideDistX < sideDistY) // 만약 x면에 부딪히는 거리가 y면에 부딪히는 거리보다 짧다면, x면으로 이동
-// 			{
-// 				sideDistX += deltaDistX; // 광선이 X 경계와 부딪혔으므로, 다음 X 경계까지의 거리를 계산하기 위해 deltaDistX를 더해줍니다.
-// 				mapX += stepX;	// mapX(광선의 현재 위치)는 stepX(1 혹은 -1)만큼 증가
-// 				side = 0;	// x면에 부딪힘
-// 			}
-// 			else						// 만약 y면에 부딪히는 거리가 x면에 부딪히는 거리보다 짧다면, y면으로 이동
-// 			{
-// 				sideDistY += deltaDistY;
-// 				mapY += stepY;
-// 				side = 1;	// y면에 부딪힘
-// 			}
-// 			if (info->config.map[mapX][mapY] != '0') 			//광선이 벽에 부딪혔는지 확인 (에러 지점: seg fault)
-// 				hit = 1; // 벽에 부딪혔다면 hit = 1
-// 		}
-// 		if (side == 0)	// 만약 x면에 부딪혔다면,
-// 			perpWallDist = (mapX - info->pos_x + (1 - stepX) / 2) / rayDirX;
-// 		else			// 만약 y면에 부딪혔다면,
-// 			perpWallDist = (mapY - info->pos_y + (1 - stepY) / 2) / rayDirY;
-
-// 		//Calculate height of line to draw on screen
-// 		int lineHeight = (int)(HEIGHT / perpWallDist);
-
-// 		//calculate lowest and highest pixel to fill in current stripe
-// 		int drawStart = -lineHeight / 2 + HEIGHT / 2; //drawStart의 역할:
-// 		if(drawStart < 0)
-// 			drawStart = 0;
-// 		int drawEnd = lineHeight / 2 + HEIGHT / 2;
-// 		if(drawEnd >= HEIGHT)
-// 			drawEnd = HEIGHT - 1;
-
-// 		// 광선이 부딪힌 벽의 방향에 따라 사용할 텍스처를 결정
-// 		// 방향에 대한 확인 필요
-// 		int texNum;
-// 		if (side == 0)   // x면에 부딪혔다면
-// 		{
-// 			if (rayDirX > 0)
-// 				texNum = 2;  // West
-// 			else
-// 				texNum = 3;  // East
-// 		}
-// 		else  			// y면에 부딪혔다면
-// 		{
-// 			if (rayDirY > 0)
-// 				texNum = 0;  // North
-// 			else
-// 				texNum = 1;  // South
-// 		}
-// 		double wallX; // wallX 계산: 벽에 부딪힌 위치 계산 (텍스처 매핑을 위해)
-// 		if (side == 0)										//만약 광선이 X면에 부딪혔다면
-// 			wallX = info->pos_y + perpWallDist * rayDirY;	//wallX는 광선이 시작된 Y 좌표와 광선의 Y 방향에 벽까지의 직교 거리를 곱한 값을 더해서 계산
-// 		else												//Y면에 부딪혔다면
-// 			wallX = info->pos_x + perpWallDist * rayDirX;	//X 좌표와 광선의 X 방향에 벽까지의 직교 거리를 곱한 값을 더해서 계산
-// 		wallX -= floor(wallX);								//그 후, wallX에서 floor(wallX)를 뺌으로써 wallX를 [0,1) 범위 안에 있게 만듭니다. 이렇게 하면 텍스처의 어느 부분이 화면에 그려져야 하는지 결정할 수 있음
-
-// 		int texX = (int)(wallX * (double)TEXTURE_WIDTH);		// 텍스처의 x 좌표 계산 (wallX 값을 텍스처의 너비로 스케일링해서 얻음)
-// 		if ((side == 0 && rayDirX > 0) || (side == 1 && rayDirY < 0)) //광선 방향에 따라서 텍스처를 뒤집을 필요 확인. 이유는 벽의 다른 쪽면에 있는 텍스처가 화면에 반대로 나타나지 않게 하기 위함
-// 			texX = TEXTURE_WIDTH - texX - 1;
-
-// 		// 화면의 각 픽셀에 대한 텍스처의 Y 좌표를 계산하기 위한 step 및 texPos 값 설정
-// 		double step = 1.0 * TEXTURE_HEIGHT / lineHeight; //step은 화면의 각 픽셀에 대해서 텍스처의 어떤 Y 좌표를 사용해야 하는지 결정하는 값의 증가량. 벽의 높이와 텍스처의 높이의 비율을 사용해 계산
-// 		double texPos = (drawStart - HEIGHT / 2 + lineHeight / 2) * step;	//texPos는 텍스처에서의 시작 Y 좌표를 나타냅니다.
-
-// 		for (int y = drawStart; y < drawEnd; y++)
-// 		{
-// 			int texY = (int)texPos & (TEXTURE_HEIGHT - 1);	//texY는 사용될 텍스처의 Y 좌표
-// 			texPos += step;									//step을 더해서 다음 픽셀의 텍스처 Y 좌표를 계산
-
-// 			int color = info->texture[texNum][TEXTURE_HEIGHT * texY + texX]; //텍스처에서 해당 좌표의 색상 값을 가져
-
-// 			if (side == 1)
-// 				color = (color >> 1) & 8355711;  // 만약 광선이 Y면에 부딪혔다면, 색상 값을 약간 어둡게 (시각효과, 필요한지 모르겠음)
-
-// 			info->buf[y][x] = color;	//그 후, 해당 화면의 X, Y 좌표에 해당하는 버퍼에 색상 값을 저장
-// 		}
-
-// 		// 그리는 부분 (temp.c에 draw에 해당)
-// 		for (int y = 0; y < HEIGHT; y++) // row (height) 훑기
-// 		{
-// 			for (int x = 0; x < WIDTH; x++)	// column (width) 훑기
-// 			{
-// 				// 화면의 2D 포인트 (x,y)를 'data' 배열의 1D 포인트로 매핑
-// 				// 5x5 크기의 2D 배열의 경우, 첫 번째 행(즉, y=0)은 1D 인덱스 0부터 4까지 매핑. 두 번째 행(y=1)은 1D 인덱스 5부터 9까지 매핑
-// 				// y * width로 계산된 시작 인덱스에 x를 더하면, 우리는 그 행에서 정확한 열의 원소의 1D 인덱스에 도달. 예: 5x5 배열에서 y=2, x=3의 2D 좌표는 2 * 5 + 3 = 13으로 1D 인덱스 13에 매핑
-// 				info->img.data[y * WIDTH + x] = info->buf[y][x];
-// 			}
-// 		}
-// 		mlx_put_image_to_window(info->mlx, info->win, info->img.img, 0, 0); // 이미지를 윈도우에 표시
-// 	}
-// 	return (0);
-// }
 
 int handle_keys(int keycode, t_info *info)
 {
