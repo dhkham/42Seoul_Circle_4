c3d15f41a7245f7f2a56c5b8285afbd33823152d
dhkham <khamdong@gmail.com>
2023-09-12 19:19:21 +0900

    remove leak check

--- cub3d/main.c
+++ cub3d/main.c
@@ -6,24 +6,17 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 20:23:55 by dkham             #+#    #+#             */
-/*   Updated: 2023/09/12 18:57:17 by dkham            ###   ########.fr       */
+/*   Updated: 2023/09/12 19:18:59 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// leak check
-void leak_check(void)
-{
-	system("leaks cub3D");
-}
 
 int	main(int argc, char **argv)
 {
 	t_config	*s_config;
 	t_info		s_info;
 
-	atexit(leak_check);
 	if (!validate_arguments(argc, argv, &s_config))
 		return (0);
 	initialize_s_info(&s_info, s_config);
