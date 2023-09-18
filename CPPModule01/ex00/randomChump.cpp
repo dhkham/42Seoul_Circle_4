41ae5a0911f0000249697b51e4a793449a93da05
dhkham <khamdong@gmail.com>
2023-09-18 18:42:45 +0900

    add comments

--- CPPModule01/ex00/randomChump.cpp
+++ CPPModule01/ex00/randomChump.cpp
@@ -6,13 +6,15 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 19:55:12 by dkham             #+#    #+#             */
-/*   Updated: 2023/09/17 19:55:15 by dkham            ###   ########.fr       */
+/*   Updated: 2023/09/18 18:33:06 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 void randomChump(std::string name) {
+	//creating an instance(=stackZombie) of the Zombie class.
+	//parentheses indicates calling the constructor of the Zombie class and passing the name argument to it
     Zombie stackZombie(name);
     stackZombie.announce();
 }
