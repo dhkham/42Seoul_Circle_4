0c7eab6bdda479c6db3ca9a6a84d3035811dca05
dhkham <khamdong@gmail.com>
2023-09-21 22:27:56 +0900

    change zombiename

--- CPPModule01/ex01/zombieHorde.cpp
+++ CPPModule01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 19:12:05 by dkham             #+#    #+#             */
-/*   Updated: 2023/09/18 19:54:43 by dkham            ###   ########.fr       */
+/*   Updated: 2023/09/21 22:26:23 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@
 Zombie* zombieHorde(int N, std::string name) {
     Zombie* horde = new Zombie[N];
     for (int i = 0; i < N; ++i) {
-        std::stringstream zombieNameWithNumber;
-        zombieNameWithNumber << name << (i + 1);  // Combining the base name with the number
-        new(&horde[i]) Zombie(zombieNameWithNumber.str()); // Placement new: construct an object(=Zombie) in the already allocated memory with the constructed name
+        new(&horde[i]) Zombie(name); // Placement new: construct an object(=Zombie) in the already allocated memory with the provided name
     }
     return horde;
 }
